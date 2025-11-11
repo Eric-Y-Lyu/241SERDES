19045af8a9f40ee811217e38c4e2f66752973670
SunnyyLiu <sunnyy.liu@mail.utoronto.ca>
2025-11-11 17:05:08 -0500

    Sunny-11-11-5:04

---
+++ SERDES_PRJ/downCount9RX.v
@@ -0,0 +1,24 @@
+module downCount9RX (in, ResetN, enable, tick);
+	input in, ResetN, enable;
+	reg [3:0] count;
+	output reg tick;
+
+	always @ (posedge in, negedge ResetN)
+	begin
+		if (ResetN == 0)
+			count <= 4'b1000;
+		else if (count == 4'b0000)
+			count <= 4'b1000;
+		else if (enable == 1)	
+			count <= count - 1;
+	end
+
+	always @ (*)
+	begin
+		if (count == 4'b0000)
+			tick = 1;
+		else 
+			tick = 0;
+	end
+
+endmodule
